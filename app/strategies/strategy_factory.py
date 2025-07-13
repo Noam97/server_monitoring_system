025904f25d10137e573939e26f4e044a00fd609e
Noam <noam@Noams-MacBook-Pro.local>
2025-07-13 12:37:12 +0300

    Add MonitorStrategyFactory to dynamically return strategy by protocol

---
+++ app/strategies/strategy_factory.py
@@ -0,0 +1,19 @@
+from strategies.http_strategy import HttpMonitorStrategy
+# from strategies.ftp_strategy import FtpMonitorStrategy
+# from strategies.ssh_strategy import SshMonitorStrategy
+
+class MonitorStrategyFactory:
+    @staticmethod
+    def get_strategy(protocol: str):
+        protocol = protocol.lower()
+        
+        if protocol in ["http", "https"]:
+            return HttpMonitorStrategy()
+        
+        # elif protocol == "ftp":
+        #     return FtpMonitorStrategy()
+        # elif protocol == "ssh":
+        #     return SshMonitorStrategy()
+        
+        else:
+            raise ValueError(f"Unsupported protocol: {protocol}")

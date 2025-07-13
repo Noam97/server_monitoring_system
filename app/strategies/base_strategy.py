7d3ceff6c22a433c1b47d43d574233ee2e232fb9
Noam <noam@Noams-MacBook-Pro.local>
2025-07-13 12:36:31 +0300

    use constant

--- app/strategies/base_strategy.py
+++ app/strategies/base_strategy.py
@@ -1,6 +1,11 @@
 from abc import ABC, abstractmethod
 
 class MonitorStrategy(ABC):
+    MAX_ALLOWED_TIME = 45.0
     @abstractmethod
     def check(self, server):
+        """
+        Run a health check on the given server.
+        This method must be implemented by any strategy.
+        """
         pass
